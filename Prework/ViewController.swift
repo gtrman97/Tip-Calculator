04bfe9eea438faf5144b813d1e0cf2abdf62e707
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-27 20:27:02 -0600

    defined function

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        // Get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
